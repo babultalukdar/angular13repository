2d282c4da36bd49d09d8080d17536cfa14622ace
babultalukdar <talukdar.mca@gmail.com>
2024-04-28 19:18:46 +0530

    commit changes

--- src/app/user/child1/child1.component.ts
+++ src/app/user/child1/child1.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child1',
   templateUrl: './child1.component.html',
   styleUrls: ['./child1.component.css']
 })
-export class Child1Component {
-
+export class Child1Component implements OnInit{
+  ngOnInit(): void {
+    console.log("hello babul");
+  }
+    
 }
